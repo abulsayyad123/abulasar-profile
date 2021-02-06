0f5d27ad1d109e6683090bdbfc642c70cd3b3122
AbulAsar Sayyad <abul.a@kiprosh.com>
2021-02-06 18:52:48 +0530

    added seperate route for fetching  blog details with graphql query

---
+++ app/routes/blogs/details.js
@@ -0,0 +1,13 @@
+import Route from '@ember/routing/route';
+import { queryManager } from "ember-apollo-client";
+import query from "abulasar-profile/gql/queries/blog-detail.graphql";
+
+export default class BlogsDetailsRoute extends Route {
+  @queryManager apollo;
+
+  async model({slug}) {
+    let variables = { slug }
+    const content = await this.apollo.watchQuery({ query, variables }, "post");
+    return content;
+  }
+}
